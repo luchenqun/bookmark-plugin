c6b56d10abefa001f89dab9702de0bf7f97f7bdf
luchenqun <luchenqun@juzix.io>
2018-04-02 19:04:37 +0800

    Update README.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # bookmark-plugin
-配合[my-bookmart](http://mybookmark.cn)快速添加书签到系统
+一个Google浏览器插件，配合[my-bookmart](http://mybookmark.cn)快速添加书签到系统。
 
 ![image](http://mybookmark.cn/images/screenshot.gif)  
--- js/content-script.js
+++ js/content-script.js
@@ -1,4 +1,4 @@
-﻿console.log('bookmark content script!');
+﻿// console.log('bookmark content script!');
 
 // 注意，必须设置了run_at=document_start 此段代码才会生效
 document.addEventListener('DOMContentLoaded', function () {
