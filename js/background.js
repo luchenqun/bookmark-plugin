c6adfc4184daba0a37d5fc7c3989eab67fb1db65
luchenqun <luchenqun@juzix.io>
2018-05-23 15:59:50 +0800

    如果contextMenus初始化失败则过60秒再试以及过滤掉emoji

--- js/background.js
+++ js/background.js
@@ -1,7 +1,6 @@
+console.log("bookmark init background")
+
 // 预留一个方法给popup调用
-
-console.log("init background...")
-
 function showMsg(body, title, time) {
 	var note = new Notification(title || "通知", {
 		dir: "auto",
@@ -90,6 +89,7 @@ function addNote(info, tab, tagId) {
 }
 
 function init() {
+	console.log("bookmark init contextMenus");
 	jqAjax("https://mybookmark.cn/api/tags/", "GET", {}, function (tags, textStatus, jqXHR) {
 		chrome.contextMenus.removeAll();
 		tags.sort((a, b) => {
@@ -138,11 +138,19 @@ function init() {
 						code: "window.getSelection().toString();"
 					}, function (selection) {
 						info.selectionText = selection[0];
+						var regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+						// 清掉emoji
+						info.selectionText = info.selectionText.replace(regex, '');
 						addNote(info, tab, tag.id);
 					});
 				}
 			});
 		}
+	}, function (xhr, textStatus) {
+		// 出错1分钟重试一次
+		setTimeout(function () {
+			init();
+		}, 60000);
 	})
 }
 
